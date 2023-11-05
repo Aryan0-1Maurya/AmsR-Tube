fa5d42543231e28eaee9d2a821d7f65a3587389e
Aryan Maurya <136960772+Aryan0-1Maurya@users.noreply.github.com>
2023-11-06 00:53:53 +0530

    Add files via upload

---
+++ server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/ubuntu/workspace/yotubecloneproject/src/app/layout":["static/media/c9a5bc6a7c948fb0-s.p.woff2","static/media/7d8c9b0ca4a64a5a-s.p.woff2","static/media/934c4b7cb736f2a3-s.p.woff2","static/media/627622453ef56b0d-s.p.woff2","static/media/0e4fe491bf84089c-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
